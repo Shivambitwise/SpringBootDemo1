f8a535d7379941c8fde53d7e6f3ea0df8c9e6548
Shivambitwise <rajputsingh4all@gmail.com>
2025-06-21 08:18:04 +0530

    Initial commit

---
+++ src/main/java/com/shivam/productserivce/Springboot/repository/ProductRepository.java
@@ -0,0 +1,4 @@
+package com.shivam.productserivce.Springboot.repository;
+
+public interface ProductRepository {
+}
---
+++ src/test/java/com/shivam/productserivce/Springboot/ServiceTests/ProductServiceTests.java
@@ -0,0 +1,4 @@
+package com.shivam.productserivce.Springboot.ServiceTests;
+
+public class ProductServiceTests {
+}
